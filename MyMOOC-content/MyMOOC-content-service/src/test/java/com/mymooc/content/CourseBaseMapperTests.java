ad5b40b443fb7211738727c34d7c5567ace13d56
Zheng Lian <ss4g0923@gmail.com>
2024-02-11 22:41:37 +0800

    Update: added program entry point and test methods

---
+++ MyMOOC-content/MyMOOC-content-service/src/test/java/com/mymooc/content/CourseBaseMapperTests.java
@@ -0,0 +1,50 @@
+package com.mymooc.content;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mymooc.base.model.PageParams;
+import com.mymooc.base.model.PageResult;
+import com.mymooc.content.mapper.CourseBaseMapper;
+import com.mymooc.content.model.dto.QueryCourseParamsDto;
+import com.mymooc.content.model.po.CourseBase;
+import org.apache.commons.lang.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class CourseBaseMapperTests {
+
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+
+    @Test
+    public void testCourseBaseMapper() {
+        CourseBase courseBase = courseBaseMapper.selectById(18);
+        Assertions.assertNotNull(courseBase);
+
+
+        QueryCourseParamsDto courseParamsDto = new QueryCourseParamsDto();
+        courseParamsDto.setCourseName("java");
+
+        LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotEmpty(courseParamsDto.getCourseName()), CourseBase::getName, courseParamsDto.getCourseName());
+        queryWrapper.eq(StringUtils.isNotEmpty(courseParamsDto.getAuditStatus()), CourseBase::getAuditStatus, courseParamsDto.getAuditStatus());
+
+        PageParams pageParams = new PageParams();
+        pageParams.setPageNo(1L);
+        pageParams.setPageSize(2L);
+        Page<CourseBase> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
+
+        Page<CourseBase> pageResult = courseBaseMapper.selectPage(page, queryWrapper);
+        List<CourseBase> items = pageResult.getRecords();
+        long total = pageResult.getTotal();
+
+        PageResult<CourseBase> courseBasePageResult = new PageResult<CourseBase>(items, total, pageParams.getPageNo(), pageParams.getPageSize());
+
+        System.out.println(courseBasePageResult);
+    }
+}
