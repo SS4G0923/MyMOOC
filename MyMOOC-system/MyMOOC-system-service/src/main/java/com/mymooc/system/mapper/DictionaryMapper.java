f52858cce94ebc362e710def96f229561ecb16cc
Zheng Lian <ss4g0923@gmail.com>
2024-02-11 22:42:57 +0800

    Update: added system service for dictionary mappings and some configuration updates

---
+++ MyMOOC-system/MyMOOC-system-service/src/main/java/com/mymooc/system/mapper/DictionaryMapper.java
@@ -0,0 +1,16 @@
+package com.mymooc.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mymooc.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
