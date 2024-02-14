440b571d1a9357efe7abd548b71e37f21c95c1d3
Zheng Lian <ss4g0923@gmail.com>
2024-02-14 22:32:37 +0800

    Update: added media management service for the project.

---
+++ MyMOOC-media/MyMOOC-media-service/src/main/java/com/mymooc/media/mapper/MediaProcessHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.mymooc.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mymooc.media.model.po.MediaProcessHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaProcessHistoryMapper extends BaseMapper<MediaProcessHistory> {
+
+}
---
+++ MyMOOC-media/MyMOOC-media-service/src/main/java/com/mymooc/media/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.mymooc.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mymooc.media.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
