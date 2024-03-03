91b05939e884089f7aad0ee219c5d8c3f3d16633
Zheng Lian <ss4g0923@gmail.com>
2024-03-03 18:22:58 +0800

    Feature: added message sdk

---
+++ MyMOOC-message-sdk/src/main/java/com/mymooc/messagesdk/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.mymooc.messagesdk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mymooc.messagesdk.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
