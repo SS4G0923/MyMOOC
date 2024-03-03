c7713d4af2a4e02aa21dff563e72cab372bc67ea
Zheng Lian <ss4g0923@gmail.com>
2024-03-03 21:47:55 +0800

    Feature: added fall back factory to handle server avalanche

--- MyMOOC-content/MyMOOC-content-service/src/main/java/com/mymooc/content/feignclient/MediaServiceClient.java
+++ MyMOOC-content/MyMOOC-content-service/src/main/java/com/mymooc/content/feignclient/MediaServiceClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@FeignClient(value = "media-api", configuration = {MultipartSupportConfig.class})
+@FeignClient(value = "media-api", configuration = {MultipartSupportConfig.class}, fallbackFactory = MediaServiceClientFallBackFactory.class)
 public interface MediaServiceClient {
 	@RequestMapping(value = "/media/upload/coursefile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public String upload(@RequestPart("filedata") MultipartFile filedata,
